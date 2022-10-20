78b697af8edda85282cf5957b769fc5b858bcf5b
Bobby Echo Prasetyo <bobbyechop@gmail.com>
2022-10-20 18:41:02 +0700

    feat: ReadWriteProperty Interface

---
+++ src/main/kotlin/_3_Generic/12. ReadWriteProperty Interface.kt
@@ -0,0 +1,25 @@
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class NameWithLogSetterGetter(param: String) {
+    var name: String by LogSetterGetter(param)
+}
+
+class LogSetterGetter(var param: String) : ReadWriteProperty<Any, String> {
+    override fun getValue(thisRef: Any, property: KProperty<*>): String {
+        println("Get data ${property.name} with value ${this.param}")
+        return this.param
+    }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: String) {
+        println("Set data ${property.name} with value $value")
+        this.param = value
+    }
+}
+
+fun main() {
+    val log = NameWithLogSetterGetter("Sarah")
+    println(log.name)
+    log.name = "Bobby"
+    println(log.name)
+}
